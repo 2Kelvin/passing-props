c2eccf86a709539ebfbdde9270180d4db096571f
2Kelvin <kvin.mwangi254@gmail.com>
2022-10-23 11:58:23 +0300

    set a default value to prop size

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 function Avatar(_ref) {
     var person = _ref.person,
-        size = _ref.size;
-
+        _ref$size = _ref.size,
+        size = _ref$size === undefined ? 120 : _ref$size;
+    //prop 'size' has been assigned a default value
     return React.createElement("img", {
         src: person.img_src,
         alt: person._name,
         width: size,
-        height: size,
+        height: "180",
         className: "avatar_img"
     });
 }
@@ -20,42 +21,42 @@ function FriendsCast() {
                 _name: "Monica Geller",
                 img_src: "../images/monica.png"
             },
-            size: 150
+            size: 130
         }),
         React.createElement(Avatar, {
             person: {
-                _name: "Monica Geller",
+                _name: "Chandler Bing",
                 img_src: "../images/chandler.png"
             },
-            size: 150
+            size: 130
         }),
         React.createElement(Avatar, {
             person: {
-                _name: "Monica Geller",
+                _name: "Ross Geller",
                 img_src: "../images/ross.png"
             },
-            size: 150
+            size: 130
         }),
         React.createElement(Avatar, {
             person: {
-                _name: "Monica Geller",
+                _name: "Rachael Green",
                 img_src: "../images/rachael.png"
             },
-            size: 150
+            size: 130
         }),
         React.createElement(Avatar, {
             person: {
-                _name: "Monica Geller",
+                _name: "Joey Tribbiani",
                 img_src: "../images/joey.png"
             },
-            size: 150
+            size: 130
         }),
         React.createElement(Avatar, {
             person: {
-                _name: "Monica Geller",
+                _name: "Phoebe Buffet",
                 img_src: "../images/phoebes.png"
             },
-            size: 150
+            size: 130
         })
     );
 }
@@ -94,3 +95,16 @@ root.render(React.createElement(App, null));
 // passing props => from parent to child
 // reading props => inside child's ({}) and using them inside it's {}
 //  you can make each component display its own unique info by changing the values of the porps passed
+// think of props like “knobs” that you can adjust
+// *** they serve the same role as arguments serve for functions
+// *** props are the only argument to your component 
+//  *** React component functions accept a single argument, a props object
+// *** Usually you don’t need the whole props object itself
+// *** so you destructure it into individual props
+//  for example, in the backscene the above props are seen as 'props.person', 'props.size'
+// but using destructuring, we use what we only need, {person}, {size}...together {person, size}
+// you can give a prop a default value to fall back on when no value is specified
+// prop 'size' above has been assigned a default value
+// ...it'll be used when the size prop is not passed on the <Avatar/> component
+// if you pass 'size={null}' or 'size={0}' the default value will not be used; the values specified are the ones that'll be passed to size prop
+// however, if 'size={undefined}' the default value will be used
